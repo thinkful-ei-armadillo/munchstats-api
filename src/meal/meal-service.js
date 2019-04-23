a5738a8a44970637aa6f0e3268d72366e92d57fe
Michael Bramble <mike.w.bramble@gmail.com>
2019-04-23 16:52:49 -0400

    meal route/service 'complete'

--- src/meal/meal-service.js
+++ src/meal/meal-service.js
@@ -6,18 +6,25 @@ const MealService = {
       .where({user_id});
   },
 
-  getMealIngredients(db, meal_id){
-    return db
-      .from('ingredient')
-      .select('*')
-      .where({meal_id});
-  },
-
   insertMeal(db, newMeal){
     return db
       .insert(newMeal)
       .into('meal')
       .returning('*')
+  },
+
+  deleteMeal(db, id){
+    return db
+      .from('meal')
+      .where({id})
+      .delete()
+  },
+
+  updateMeal(db, id, payload){
+    return db
+      .from('meal')
+      .where({id})
+      .update(payload)
   }
 };
 
