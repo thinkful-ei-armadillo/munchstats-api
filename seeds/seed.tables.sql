50f059916e432befda496b21203c0e048ef59ecc
Michael Bramble <mike.w.bramble@gmail.com>
2019-04-23 11:58:53 -0400

    wrote seed data

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -12,3 +12,16 @@ VALUES
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
+
+INSERT INTO "meal" ("id", "name", "user_id")
+VALUES
+  (1, 'Example Meal 1', 1);
+
+INSERT INTO "ingredient" ("id", "name", "meal_id")
+VALUES
+  (1, 'Example Ingredient 1', 1),
+  (2, 'Example Ingredient 2', 1),
+  (3, 'Example Ingredient 3', 1),
+  (4, 'Example Ingredient 4', 1);
+
+COMMIT;
