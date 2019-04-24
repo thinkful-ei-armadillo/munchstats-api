75f75e473827285f03c17ce6fe307c0c2d2c7289
Geordie Connell <geordie.connell@gmail.com>
2019-04-24 11:18:33 -0400

    seed file all set up!

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -3,7 +3,7 @@ BEGIN;
 -- psql -U postgres -d munchstats -f seeds/seed.tables.sql
 
 TRUNCATE
-  "user";
+  "user", "meal", "ingredients";
 
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
@@ -19,11 +19,11 @@ INSERT INTO "meal" ("id", "name", "user_id")
 VALUES
   (1, 'Example Meal 1', 1);
 
-INSERT INTO "ingredient" ("id", "name", "meal_id")
+INSERT INTO "ingredients" ("id", "name", "meal_id", "unit")
 VALUES
-  (1, 'Example Ingredient 1', 1),
-  (2, 'Example Ingredient 2', 1),
-  (3, 'Example Ingredient 3', 1),
-  (4, 'Example Ingredient 4', 1);
+  (1, 'Example Ingredient 1', 1, 'kg'),
+  (2, 'Example Ingredient 2', 1, 'L'),
+  (3, 'Example Ingredient 3', 1, 'lbs'),
+  (4, 'Example Ingredient 4', 1, 'g');
 
 COMMIT;
