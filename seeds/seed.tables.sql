03e1387b2d444bde7e7a096ae5ce8c0f268e861c
Geordie Connell <geordie.connell@gmail.com>
2019-05-01 17:53:54 -0400

    added more info into seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -27,14 +27,15 @@ VALUES
   (3, 'Example Ingredient 3', 1, 'lbs'),
   (4, 'Example Ingredient 4', 1, 'g');
 
-INSERT INTO "events" ("id", "user_id", "name", "date", "tag")
+INSERT INTO "events" ("id", "user_id", "name", "date", "tag", "calories", "protein", "fat", "carbs")
 VALUES
-  (1, 1, 'monday breakfast', '2019-04-29 09:00:00', 'breakfast'),
-  (2, 1, 'monday lunch', '2019-04-29 13:00:00', 'lunch'),
-  (3, 1, 'easter brunch', '2019-04-29 09:00:00', 'brunch'),
-  (4, 1, 'monday dinner', '2019-04-29 20:00:00', 'dinner'),
-  (5, 1, 'christmas dinner', '2018-12-25 19:00:00', 'dinner'),
-  (6, 1, 'tuesday breakfast', '2019-04-30 09:00:00', 'breakfast'),
-  (7, 1, 'sunday brunch', '2019-04-28 09:00:00', 'brunch');
+  (1, 1, 'monday breakfast', '2019-04-29 09:00:00', 'breakfast', 350, 12, 3, 20),
+  (2, 1, 'monday lunch', '2019-04-29 13:00:00', 'lunch', 500, 20, 10, 15),
+  (3, 1, 'easter brunch', '2019-04-29 09:00:00', 'brunch', 700, 22, 15, 30),
+  (4, 1, 'monday dinner', '2019-04-29 20:00:00', 'dinner', 600, 30, 5, 5),
+  (5, 1, 'christmas dinner', '2018-12-25 19:00:00', 'dinner', 1000, 40, 15, 40),
+  (6, 1, 'tuesday breakfast', '2019-04-30 09:00:00', 'breakfast', 350, 12, 3, 20),
+  (7, 1, 'sunday brunch', '2019-04-28 09:00:00', 'brunch', 500, 30, 15, 25),
+  (8, 1, 'wednesday breakfast', '2019-05-01 09:00:00', 'breakfast', 350, 12, 3, 20);
 
 COMMIT;
