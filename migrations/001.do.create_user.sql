9fe8c309814067229b4444dea1e7574f1843dcb2
Michael Bramble <mike.w.bramble@gmail.com>
2019-05-02 16:14:13 -0400

    added columns in user table to account for user budget settings

--- migrations/001.do.create_user.sql
+++ migrations/001.do.create_user.sql
@@ -2,5 +2,11 @@ CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
   "username" TEXT NOT NULL UNIQUE,
   "password" TEXT NOT NULL,
-  "name" TEXT NOT NULL
+  "name" TEXT NOT NULL,
+  "isDark" BOOLEAN DEFAULT FALSE,
+  "miami" BOOLEAN DEFAULT FALSE,
+  "calorieBudget" INTEGER,
+  "fatBudget" INTEGER,
+  "carbBudget" INTEGER,
+  "proteinBudget" INTEGER
 );
